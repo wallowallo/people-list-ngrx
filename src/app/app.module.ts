028bc0f1cebfc8534765e46106f65b07ad079565
Krister <krister.torsvik@gmail.com>
2017-05-03 10:57:00 +0200

    added ngrx store and core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore({})
   ],
   providers: [],
   bootstrap: [AppComponent]
